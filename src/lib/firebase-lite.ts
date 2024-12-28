701520bcd15042fb6a2598e06acfdba4c41f7d54
Jonathan Gamble <jdgamble555@gmail.com>
2024-12-28 09:11:48 -0600

    log

--- src/lib/firebase-lite.ts
+++ src/lib/firebase-lite.ts
@@ -10,6 +10,8 @@ export const firebaseServer = async (request: Request) => {
 
     const authIdToken = request.headers.get('Authorization')?.split('Bearer ')[1];
 
+    console.log(authIdToken);
+
     const serverApp = initializeServerApp(firebase_config, {
         authIdToken
     });
